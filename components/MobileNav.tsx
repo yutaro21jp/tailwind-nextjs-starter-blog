634b63e6a5af0df57c953bac138427e8162e589e
Timothy <timothy0336@hotmail.com>
2025-06-29 14:31:37 +0800

    Remove outdated outline style

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -76,7 +76,7 @@ const MobileNav = () => {
                   <Link
                     key={link.title}
                     href={link.href}
-                    className="hover:text-primary-500 dark:hover:text-primary-400 mb-4 py-2 pr-4 text-2xl font-bold tracking-widest text-gray-900 outline outline-0 dark:text-gray-100"
+                    className="hover:text-primary-500 dark:hover:text-primary-400 mb-4 py-2 pr-4 text-2xl font-bold tracking-widest text-gray-900 outline-0 dark:text-gray-100"
                     onClick={onToggleNav}
                   >
                     {link.title}
