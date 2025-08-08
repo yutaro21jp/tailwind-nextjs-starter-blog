7b98b9b93988f23b800f9adb0d9e9ef3560f4f78
yutaro <131897266+yutaro21jp@users.noreply.github.com>
2025-08-09 08:37:49 +0900

    ファビコン等の画像を変更しました。

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -8,7 +8,7 @@ const siteMetadata = {
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
-  siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
+  siteLogo: '/static/images/yutaro.JPG',
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   mastodon: 'https://mastodon.social/@mastodonuser',
   email: 'yutaro@tutamail.com',
